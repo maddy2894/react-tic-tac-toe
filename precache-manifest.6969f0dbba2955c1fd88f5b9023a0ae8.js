c6a9f7398d8873b10a1c240fb369914a26b41dc2
Madhavan Sundararaj <madhavansundararaj@gmail.com>
2019-03-16 21:07:56 +1100

    Updates

---
+++ precache-manifest.6969f0dbba2955c1fd88f5b9023a0ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.140d55ac.js"
+  },
+  {
+    "revision": "b359dc9811e3c43ae4a3",
+    "url": "/react-tic-tac-toe/static/js/main.b359dc98.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/react-tic-tac-toe/static/js/2.d14408cb.chunk.js"
+  },
+  {
+    "revision": "b359dc9811e3c43ae4a3",
+    "url": "/react-tic-tac-toe/static/css/main.3a6b2af3.chunk.css"
+  },
+  {
+    "revision": "900f66e8d7a439a35691b9a861efae73",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
